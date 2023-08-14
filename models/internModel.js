7ddd3b452f394b59d9b61b56299eb3fc34dd3513
abhishek <abhishek620sah@gmail.com>
2023-08-14 20:23:21 +0530

    adding validator

--- models/internModel.js
+++ models/internModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-
+const ObjectId = mongoose.Schema.Types.ObjectId;
 const internsSchema = new mongoose.Schema({
 
     name: {type: String,required: true,trim: true},
-    email: {type: String,required: true,trim: true,uniqe: true},
-    mobile: {type: String,required: true,trim: true,unique: true},
-    collegeId: {type: String,required: true,trim: true},
+    email: {type: String,required: true,trim: true,uniqe: true,lowercase: true},
+    mobile: {type: Number,required: true,trim: true,unique: true},
+    collegeId: {type: ObjectId,ref: "collegeModel",required: true},
     isDeleted: {type:Boolean,default:false},
 
     }
