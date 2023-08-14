ab2fa6b08cc7ff84c4dd45d4b38fc471d66c0299
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-08-14 19:21:04 +0530

    Adding Get API

--- routes/route.js
+++ routes/route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const collegeController = require('../controllers/collegeContoller.js');
 const internController = require('../controllers/internController.js');
+const getCollegeController = require('../controllers/getCollegeController.js');
 
 
 router.post('/colleges', collegeController.createCollege);
 router.post('/interns', internController.createIntern);
+router.get('/collegeDetails', getCollegeController.getCollegeDetail);
 
 module.exports = router; 
